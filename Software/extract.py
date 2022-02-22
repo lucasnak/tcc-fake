32261be28a855abb3d6d939915a9032c50c0819a
Vitor Duchini <vitorduch@usp.br>
2022-02-22 20:21:25 -0300

    mexendo no pos_spacy

--- Software/extract.py
+++ Software/extract.py
@@ -2,7 +2,7 @@ import os
 import sys
 import argparse
 import logging
-from preprocess import liwc, bow, pos, syntax, metrics
+from preprocess import liwc, bow, pos_spacy, syntax, metrics
 import numpy as np
 import pandas as pd
 
